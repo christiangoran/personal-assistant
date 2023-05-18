1beaf9fecb3cd62838d6dd95979e6036b9764f0a
christiangoran <>
2023-05-18 16:48:15 +0000

    Connect APIs and confirm access to google and chatGPT

--- run.py
+++ run.py
@@ -1,3 +1,33 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import json
+import requests
+import gspread
+import openai
+from google.oauth2.service_account import Credentials
+
+f = open('creds.json', 'r')
+gpt_creds = json.load(f)
+gpt_api = gpt_creds['gpt_api']
+
+openai.api_key = gpt_api
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("chat-log")
+
+log = SHEET.worksheet("log")
+
+data = log.get_all_values()
+
+completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "assistant", "content": "When did the Vietnam war end?"}]
+)
+
+print(completion)
